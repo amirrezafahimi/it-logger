0d9a355e50ae6773b9e9f859c239200df1a9a38c
Amirreza Fahimi <amirrezafahimi98@gmail.com>
2021-08-05 17:04:22 +0430

    added functionality for the other two floating buttons and started to implement redux

--- src/App.js
+++ src/App.js
@@ -2,8 +2,16 @@ import './App.css';
 import React, {useEffect, Fragment} from "react";
 import "materialize-css/dist/css/materialize.min.css";
 import M from "materialize-css/dist/js/materialize.min.js";
+
 import SearchBar from "./components/layout/SearchBar";
 import Logs from "./components/log/Logs";
+import AddBtn from "./components/layout/AddBtn";
+import AddLogModal from "./components/log/AddLogModal";
+import EditLogModal from "./components/log/EditLogModal";
+import AddTechModal from "./components/techs/AddTechModal";
+import TechListModal from "./components/techs/TechListModal";
+import {Provider} from "react-redux";
+import store from "./store";
 
 const App = () => {
     useEffect(() => {
@@ -12,12 +20,19 @@ const App = () => {
     });
 
     return (
-        <Fragment>
-            <SearchBar/>
-            <div className="container">
-                <Logs/>
-            </div>
-        </Fragment>
+        <Provider store={store}>
+            <Fragment>
+                <SearchBar/>
+                <div className="container">
+                    <AddBtn/>
+                    <AddLogModal/>
+                    <EditLogModal/>
+                    <AddTechModal/>
+                    <TechListModal/>
+                    <Logs/>
+                </div>
+            </Fragment>
+        </Provider>
     );
 }
 
